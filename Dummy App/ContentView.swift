9af47b71ef173a22ee52ecc9bf471547821cb151
Kashyap <kashyapbhatt7193@gmail.com>
2022-05-06 22:02:19 +0530

    First commit

---
+++ Dummy App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dummy App
+//
+//  Created by Kashyap Bhatt on 06/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dummy App/Dummy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy_AppApp.swift
+//  Dummy App
+//
+//  Created by Kashyap Bhatt on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Dummy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
